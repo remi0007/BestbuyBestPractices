6c1bfd3c8b25144d9f302bc39871e2e77e3416b8
Remi Douah <remidouah@gmail.com>
2020-12-01 13:49:37 -0600

    Add project files.

---
+++ BestbuyBestPractices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestbuyBestPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
