dd11debf5b668a07e63543704eb451e6733748fd
Remi Douah <remidouah@gmail.com>
2020-12-02 22:54:31 -0600

    Phase 1 added

--- BestbuyBestPractices/Program.cs
+++ BestbuyBestPractices/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.IO;
 
 namespace BestbuyBestPractices
 {
@@ -6,7 +10,13 @@ namespace BestbuyBestPractices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
         }
     }
 }
